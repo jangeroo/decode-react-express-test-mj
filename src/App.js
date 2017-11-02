63c20f48a4bf99671a64101761205296b4710227
Michael Jang <michaeljang86@gmail.com>
2017-11-02 18:07:20 -0400

    React connected to Express

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const expressApp = 'https://decode-express-test-mj.herokuapp.com/random'
+const expressApp = 'https://decode-express-test-mj.herokuapp.com'
+// const expressApp = 'http://localhost:5000'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  componentDidMount() {
+    fetch(`${expressApp}/random`)
+      .then(response => response.text())
+      .then(text => this.setState({text: text}))
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,6 +27,10 @@ class App extends Component {
         <p className="App-intro">
           This React app connects to the Express app at {expressApp}.
         </p>
+        <div>ExpressApp says:</div>
+        <div style={{'borderStyle': 'solid'}}>
+          {this.state.text}
+        </div>
       </div>
     );
   }
